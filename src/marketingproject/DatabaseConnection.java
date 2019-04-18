815dcd137e85c6239ae97759902b552c75e4f30b
Hatice Menteşe <haticemntse@gmail.com>
2019-04-18 23:58:35 +0300

    signup eklendi

--- src/marketingproject/DatabaseConnection.java
+++ src/marketingproject/DatabaseConnection.java
@@ -1,7 +1,6 @@
 package marketingproject;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.SQLException;
-//import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,34 +16,14 @@ public class DatabaseConnection {
             datasource.setPort(3306);
             datasource.setDatabaseName("market");
             connection = datasource.getConnection();
-            System.err.println("Deneme");
             }catch(SQLException ex){
                 Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public boolean AdminLogin(String UserName,String Password){
-        boolean result = false;
-        String SQL = "SELECT * FROM employee WHERE employeeUserName = ? AND employeePassword = ?";
-        try{
-            PreparedStatement statement = connection.prepareStatement(SQL,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-            statement.setString(1,UserName);
-            statement.setString(2,Password);
-            ResultSet rsLocal = statement.executeQuery();
-            rsLocal.absolute(0);
-            if(rsLocal.next()){
-              result = true;  
-            }
-            return result;
-        }catch(SQLException ex){
-                Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return result;
-    } 
-    
     public boolean UserLogin(String UserName,String Password){
         boolean result = false;
-        String SQL = "SELECT * FROM user WHERE UserEmail = ? AND UserPassword = ?";
+        String SQL = "SELECT * FROM user WHERE Username = ? AND Password = ?";
         try{
             PreparedStatement statement = connection.prepareStatement(SQL,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
             statement.setString(1,UserName);
@@ -59,4 +38,30 @@ public class DatabaseConnection {
         }
         return result;
     }
+    
+    public boolean Signup(String firstname, String lastname,String username, String password, String email,String country,String city,String address,String phone){
+        boolean result = false;
+        String SQL = "CALL RegisterUser(?,?,?,?,?,?,?,?,?);";
+        System.out.println("signup çalıştı 65 db");
+        try{
+            System.out.println("signup çalıştı 67 db");
+            PreparedStatement statement = connection.prepareStatement(SQL,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            statement.setString(1,firstname);
+            statement.setString(2,lastname);
+            statement.setString(3,username);
+            statement.setString(4,password);
+            statement.setString(5,address);
+            statement.setString(6,city);
+            statement.setString(7,phone);
+            statement.setString(8,country);
+            statement.setString(9,email);
+            
+            statement.executeQuery();
+            return true;
+        }catch(SQLException ex){
+                Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }catch(Exception ex){
+        }
+        return result;
+    } 
 }

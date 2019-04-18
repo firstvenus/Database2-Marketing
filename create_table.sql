c3272ac0434f9dc2296ca8abd4d075f15e72b937
Hatice Menteşe <haticemntse@gmail.com>
2019-04-18 21:45:58 +0300

    user tablosu güncellendi

--- create_table.sql
+++ create_table.sql
@@ -4,15 +4,15 @@ use market;
 
 CREATE TABLE user(
 	ID INT AUTO_INCREMENT PRIMARY KEY,
-    Firstname VARCHAR(50),
-    Lastname VARCHAR(50),
-    Username VARCHAR(50),
-    Password VARCHAR(20),
+    Firstname VARCHAR(50) NOT NULL,
+    Lastname VARCHAR(50) NOT NULL,
+    Username VARCHAR(50) NOT NULL,
+    Password VARCHAR(20) NOT NULL,
     Address VARCHAR(200),
     City VARCHAR(50),
     Phone VARCHAR(12),
     Country VARCHAR(50),
-    Email VARCHAR(100),
+    Email VARCHAR(100) NOT NULL,
     RegisterDate Datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=INNODB;
 

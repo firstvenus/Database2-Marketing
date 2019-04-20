ea0d402ddd636d4b2fddc69846d9c83b0078d44d
firstvenus <ayberk.eorten@gmail.com>
2019-04-20 18:08:37 +0300

    view_order_detail eklendi

--- create_view.sql
+++ create_view.sql
@@ -1,9 +1,19 @@
 use market;
 
+-- Onaylanmamış siparişleri görüntüle --
 CREATE VIEW view_incompleted_orders AS
 	SELECT
 		orders.*,
-		user.Username
+		user.Username,
+        (SELECT Count(*) FROM order_detail WHERE Order_ID=orders.ID) as OrderCount    
 	FROM orders 
 		INNER JOIN user ON user.ID = orders.User_ID
     WHERE NOT Is_Completed;
+    
+-- Order detaylarını görüntüle --
+CREATE VIEW view_order_detail AS
+	SELECT 
+		od.Order_ID ,od.Product_ID,c.Category,p.ProductName, od.Price, od.KDV 
+	FROM order_detail AS od
+		INNER JOIN product as p ON  p.ID = od.Product_ID
+		INNER JOIN category as c ON c.ID = p.Category_ID;

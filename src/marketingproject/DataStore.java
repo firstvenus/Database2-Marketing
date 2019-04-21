933c4e50c1e05f617755c000ce010a5b1b194588
Hatice Menteşe <haticemntse@gmail.com>
2019-04-21 12:34:04 +0300

    Sepete ürün ekleme fonksiyonları ve listenerlar eklendi

--- src/marketingproject/DataStore.java
+++ src/marketingproject/DataStore.java
@@ -1,6 +1,10 @@
 package marketingproject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import marketingproject.models.Employee;
+import marketingproject.models.Product;
 import marketingproject.models.User;
 
 public class DataStore {
@@ -8,9 +12,10 @@ public class DataStore {
 
     private Employee employee;
     private User user;
+    private final ArrayList<Product> cart_products = new ArrayList<>();
+    private final List<CartListener> listeners = new ArrayList<CartListener>();
 
     private DataStore() {
-
     }
 
     public static DataStore getInstance() {
@@ -36,4 +41,41 @@ public class DataStore {
     public User getUser(){
         return user;
     }
+
+    public void addToCart(Product product) {
+        cart_products.add(product);
+
+        listeners.forEach((listener) -> {
+            listener.productAdded(cart_products, product);
+        });
+    }
+    
+    public ArrayList<Product> getCartProducts(){
+        return cart_products;
+    }
+        
+    public void removeFromCart(Product product) {
+        for (int i = 0; i < cart_products.size(); i++) {
+            if(Objects.equals(cart_products.get(i).ID, product.ID)){
+                cart_products.remove(i);
+            }
+        }
+        
+        listeners.forEach((listener) -> {
+            listener.productRemoved(cart_products, product);
+        });
+    }
+    
+    public void addListener(CartListener listener){
+        listeners.add(listener);
+    }
+
+    public boolean IsProductInCart(Integer id){
+        for (int i = 0; i < cart_products.size(); i++) {
+            if(cart_products.get(i).ID == id){
+                return true;
+            }
+        }
+        return false;
+    };
 }

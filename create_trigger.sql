62469395d6d5de01dc1ef9d6c6ca27e0af515688
Hatice Menteşe <haticemntse@gmail.com>
2019-04-21 12:28:11 +0300

    Trigger eklendi

--- create_trigger.sql
+++ create_trigger.sql
@@ -10,3 +10,17 @@ BEGIN
 	WHERE ID = NEW.Product_ID;
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER after_insert_order_detail
+	AFTER INSERT ON order_detail
+    FOR EACH ROW
+BEGIN
+	UPDATE orders
+		SET Total_Price = Total_Price + NEW.Price, Total_KDV= Total_KDV + NEW.KDV
+	WHERE ID = NEW.Order_ID;
+    UPDATE product
+		SET Quantity = Quantity - 1
+	WHERE ID = NEW.Product_ID;
+END$$
+DELIMITER ;
